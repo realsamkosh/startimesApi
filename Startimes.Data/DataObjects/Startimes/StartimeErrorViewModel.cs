53b877876e5d6eb07ebd3b463c832b957aa36849
Akosile Samuel <samuel_a2@bts.com.ng>
2023-11-13 14:57:51 +0100

    fixes

---
+++ Startimes.Data/DataObjects/Startimes/StartimeErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startimes.Data.DataObjects.Startimes
+{
+    public class StartimeErrorViewModel
+    {
+        public string? ErrorCode { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
--- Startimes.Service/Modules/StartTimes/Handler/SubscriberService.cs
+++ Startimes.Service/Modules/StartTimes/Handler/SubscriberService.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using RestSharp;
 using RPNL.Net.Utilities.ResponseUtil;
 using Startimes.Data.DataObjects;
+using Startimes.Data.DataObjects.Startimes;
 using Startimes.Data.DataObjects.Startimes.Subscriber;
 using Startimes.Service.Modules.StartTimes.Interface;
 
@@ -24,12 +25,15 @@ namespace Startimes.Service.Modules.StartTimes.Handler
             ResponseModel<SubscriberViewModel> responseModel = new();
             try
             {
-                var client = new RestClient($"http://{_settings.StartimeSettings.BaseUrl}/api-payment-service/v1/subscribers/{serviceCode}");
+                var client = new RestClient($"{_settings.StartimeSettings.BaseUrl}/api-payment-service/v1/subscribers/{serviceCode}");
                 var request = new RestRequest();
+                // Add Basic Authentication header
+                string credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{_settings.StartimeSettings.Username}:{_settings.StartimeSettings.Password}"));
+                request.AddHeader("Authorization", $"Basic {credentials}");
                 request.AddHeader("co", $"{_settings.StartimeSettings.Co}");
                 request.AddHeader("accept", "application/json");
                 request.AddHeader("content-type", "application/json");
-                RestResponse response = client.ExecutePostAsync(request).GetAwaiter().GetResult();
+                RestResponse response = client.ExecuteGetAsync(request).GetAwaiter().GetResult();
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -39,20 +43,14 @@ namespace Startimes.Service.Modules.StartTimes.Handler
                     responseModel.code = ErrorCodes.Successful;
                     return responseModel;
                 }
-                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                else
                 {
-                    var errorResult1 = JsonConvert.DeserializeObject<string>(response.Content);
+                    var errorResult1 = JsonConvert.DeserializeObject<StartimeErrorViewModel>(response.Content);
                     responseModel.success = false;
-                    responseModel.message = errorResult1;
+                    responseModel.message = errorResult1.ErrorCode;
                     responseModel.code = ErrorCodes.Failed;
                     return responseModel;
                 }
-                var errorResult = JsonConvert.DeserializeObject<string>(response.Content);
-                responseModel.success = false;
-                responseModel.data = null;
-                responseModel.message = "Validation FAILED";
-                responseModel.code = ErrorCodes.Failed;
-                return responseModel;
             }
             catch (Exception ex)
             {
@@ -69,12 +67,15 @@ namespace Startimes.Service.Modules.StartTimes.Handler
             ResponseModel<SubscriberRechargeViewModel> responseModel = new();
             try
             {
-                var client = new RestClient($"http://{_settings.StartimeSettings.BaseUrl}/api-payment-service/v1/subscribers/{serviceCode}/recharge-infos");
+                var client = new RestClient($"{_settings.StartimeSettings.BaseUrl}/api-payment-service/v1/subscribers/{serviceCode}/recharge-infos");
                 var request = new RestRequest();
+                // Add Basic Authentication header
+                string credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{_settings.StartimeSettings.Username}:{_settings.StartimeSettings.Password}"));
+                request.AddHeader("Authorization", $"Basic {credentials}");
                 request.AddHeader("co", $"{_settings.StartimeSettings.Co}");
                 request.AddHeader("accept", "application/json");
                 request.AddHeader("content-type", "application/json");
-                RestResponse response = client.ExecutePostAsync(request).GetAwaiter().GetResult();
+                RestResponse response = client.ExecuteGetAsync(request).GetAwaiter().GetResult();
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -84,21 +85,15 @@ namespace Startimes.Service.Modules.StartTimes.Handler
                     responseModel.code = ErrorCodes.Successful;
                     return responseModel;
                 }
-                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                else
                 {
-                    var errorResult = JsonConvert.DeserializeObject<string>(response.Content);
+                    var errorResult = JsonConvert.DeserializeObject<StartimeErrorViewModel>(response.Content);
                     responseModel.success = false;
                     responseModel.data = null;
-                    responseModel.message = errorResult;
+                    responseModel.message = errorResult.ErrorCode;
                     responseModel.code = ErrorCodes.Failed;
                     return responseModel;
                 }
-
-                responseModel.success = false;
-                responseModel.data = null;
-                responseModel.message = "Validation FAILED";
-                responseModel.code = ErrorCodes.Failed;
-                return responseModel;
             }
             catch (Exception ex)
             {
@@ -115,12 +110,15 @@ namespace Startimes.Service.Modules.StartTimes.Handler
             ResponseModel<SubscriberReplaceablePackageViewModel> responseModel = new();
             try
             {
-                var client = new RestClient($"http://{_settings.StartimeSettings.BaseUrl}/api-payment-service/v1/subscribers/{serviceCode}/replaceable-packages");
+                var client = new RestClient($"{_settings.StartimeSettings.BaseUrl}/api-payment-service/v1/subscribers/{serviceCode}/replaceable-packages");
                 var request = new RestRequest();
+                // Add Basic Authentication header
+                string credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{_settings.StartimeSettings.Username}:{_settings.StartimeSettings.Password}"));
+                request.AddHeader("Authorization", $"Basic {credentials}");
                 request.AddHeader("co", $"{_settings.StartimeSettings.Co}");
                 request.AddHeader("accept", "application/json");
                 request.AddHeader("content-type", "application/json");
-                RestResponse response = client.ExecutePostAsync(request).GetAwaiter().GetResult();
+                RestResponse response = client.ExecuteGetAsync(request).GetAwaiter().GetResult();
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -130,21 +128,15 @@ namespace Startimes.Service.Modules.StartTimes.Handler
                     responseModel.code = ErrorCodes.Successful;
                     return responseModel;
                 }
-                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                else
                 {
-                    var errorResult1 = JsonConvert.DeserializeObject<string>(response.Content);
+                    var errorResult = JsonConvert.DeserializeObject<StartimeErrorViewModel>(response.Content);
                     responseModel.success = false;
                     responseModel.data = null;
-                    responseModel.message = errorResult1;
+                    responseModel.message = errorResult.ErrorCode;
                     responseModel.code = ErrorCodes.Failed;
                     return responseModel;
                 }
-                var errorResult = JsonConvert.DeserializeObject<string>(response.Content);
-                responseModel.success = false;
-                responseModel.data = null;
-                responseModel.message = errorResult;
-                responseModel.code = ErrorCodes.Failed;
-                return responseModel;
             }
             catch (Exception ex)
             {
