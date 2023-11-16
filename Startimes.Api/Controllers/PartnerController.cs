c6b38c7ca76ba29a9d201cd042433e59bb074049
Akosile Samuel <samuel_a2@bts.com.ng>
2023-11-16 20:50:48 +0100

    fixed

--- Startimes.Api/Controllers/PartnerController.cs
+++ Startimes.Api/Controllers/PartnerController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RPNL.Net.Utilities.ResponseUtil;
+using Startimes.Data.DataObjects.Recharge;
+using Startimes.Service.Modules.StartTimes.Handler;
+using Startimes.Service.Modules.StartTimes.Interface;
 
 namespace Startimes.Api.Controllers
 {
@@ -6,5 +10,47 @@ namespace Startimes.Api.Controllers
     [ApiController]
     public class PartnerController : BaseController
     {
+        private readonly IPartnerService _partnerService;
+
+        public PartnerController(IPartnerService partnerService)
+        {
+            _partnerService = partnerService;
+        }
+
+        [HttpGet("get-transactions/{serialNo}")]
+        public IActionResult GetTransactions(string serialNo)
+        {
+            if (!ModelState.IsValid)
+            {
+                NotifyModelStateError();
+            }
+
+            var result = _partnerService.GetPartnersTransactions(serialNo);
+            return Response(new ResponseModel
+            {
+                code = result.code,
+                message = result.message,
+                data = result.data,
+                success = result.success,
+            });
+        }
+
+        [HttpGet("get-accounts")]
+        public IActionResult GetAccounts()
+        {
+            if (!ModelState.IsValid)
+            {
+                NotifyModelStateError();
+            }
+
+            var result = _partnerService.GetPartnersAccounts();
+            return Response(new ResponseModel
+            {
+                code = result.code,
+                message = result.message,
+                data = result.data,
+                success = result.success,
+            });
+        }
     }
 }
